95bb82af6fcae8cc35b9ee6ee8b53ea9d91984be
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-08-08 20:57:45 +0600

    authorisation to users

--- routes/auth.js
+++ routes/auth.js
@@ -4,16 +4,18 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("./validation/registerValidation");
 const jwt = require("jsonwebtoken");
-// @route GET api/auth/test
-// @desc Test auth route
-// @access Public
+const requiresAuth = require("../middleware/permissions");
+
+// @route   GET /api/auth/test
+// @desc    Test the auth route
+// @access  Public
 router.get("/test", (req, res) => {
-  res.send("Auth route");
+  res.send("Auth route working");
 });
 
-// @route POST api/auth/register
-// @desc Create a new user
-// @access Public
+// @route   POST /api/auth/register
+// @desc    Create a new user
+// @access  Public
 router.post("/register", async (req, res) => {
   try {
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -21,17 +23,21 @@ router.post("/register", async (req, res) => {
     if (!isValid) {
       return res.status(400).json(errors);
     }
-    // Check if user already exists
+
+    // check for existing user
     const existingEmail = await User.findOne({
-      email: new RegExp(`^${req.body.email}$`, "i"),
+      email: new RegExp("^" + req.body.email + "$", "i"),
     });
 
     if (existingEmail) {
-      return res.status(400).json({ error: "User exists with this email" });
+      return res
+        .status(400)
+        .json({ error: "There is already a user with this email" });
     }
 
     // hash the password
     const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
     // create a new user
     const newUser = new User({
       email: req.body.email,
@@ -39,7 +45,7 @@ router.post("/register", async (req, res) => {
       name: req.body.name,
     });
 
-    // save user to database
+    // save the user to the database
     const savedUser = await newUser.save();
 
     const userToReturn = { ...savedUser._doc };
@@ -47,7 +53,7 @@ router.post("/register", async (req, res) => {
 
     // return the new user
     return res.json(userToReturn);
-  } catch {
+  } catch (err) {
     // error here
     console.log(err);
 
@@ -55,14 +61,14 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// @route POST api/auth/login
-// @desc Login user and return a access token
-// @access Public
+// @route   POST /api/auth/login
+// @desc    Login user and return a access token
+// @access  Public
 router.post("/login", async (req, res) => {
   try {
-    // check if user exists
+    // check for the user
     const user = await User.findOne({
-      email: new RegExp(`^${req.body.email}$`, "i"),
+      email: new RegExp("^" + req.body.email + "$", "i"),
     });
 
     if (!user) {
@@ -84,28 +90,39 @@ router.post("/login", async (req, res) => {
 
     const payload = { userId: user._id };
 
-    const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
 
-    res.cookie("acces-token", token, {
+    res.cookie("access-token", token, {
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
     });
+
     const userToReturn = { ...user._doc };
     delete userToReturn.password;
 
     return res.json({
-      token,
+      token: token,
       user: userToReturn,
     });
-  } catch {
-    // error here
+  } catch (err) {
     console.log(err);
 
-    res.status(500).send(err.message);
+    return res.status(500).send(err.message);
   }
+});
+
+// @route   GET /api/auth/current
+// @desc    Return the currently authed user
+// @access  Private
+router.get("/current", requiresAuth, (req, res) => {
+  if (!req.user) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  return res.json(req.user);
 });
 
 module.exports = router;
