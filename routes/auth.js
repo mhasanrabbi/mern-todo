cc7a63bc9caa1bab74556e6c1fb0210d181bc111
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-08-09 19:05:14 +0600

    logout route

--- routes/auth.js
+++ routes/auth.js
@@ -137,4 +137,18 @@ router.get("/current", requiresAuth, (req, res) => {
   return res.json(req.user);
 });
 
+// @route   PUT /api/auth/logout
+// @desc    Logut the user and clear the cookie
+// @access  Private
+router.put("/logout", requiresAuth, async (req, res) => {
+  try {
+    res.clearCookie("access-token");
+
+    return res.json({ success: true });
+  } catch (error) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  }
+});
+
 module.exports = router;
