a9c2f7d195e25fd095553f1ff089610ada256909
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-08-10 13:21:30 +0600

    set states of auth box form

--- client/src/components/AuthBox.js
+++ client/src/components/AuthBox.js
@@ -1,35 +1,99 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useGlobalContext } from "../context/GlobalContext";
 
 const AuthBox = ({ register }) => {
+  const { getCurrentUser } = useGlobalContext();
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [loading, setLoading] = React.useState("");
+  const [errors, setErrors] = React.useState({});
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    let data = {};
+
+    if (register) {
+      data = {
+        name,
+        email,
+        password,
+        confirmPassword,
+      };
+    } else {
+      data = {
+        email,
+        password,
+      };
+    }
+
+    axios
+      .post(register ? "api/auth/register" : "api/auth/login", data)
+      .then(() => {
+        // TODO
+        getCurrentUser();
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err?.response?.data) {
+          setErrors(err.response.data);
+        }
+      });
+  };
+
   return (
     <div className="auth">
       <div className="auth__box">
         <div className="auth__header">
           <h1>{register ? "Register" : "Login"}</h1>
         </div>
-        <form>
+        <form onSubmit={onSubmit}>
           {register && (
             <div className="auth__field">
               <label htmlFor="name">Name</label>
-              <input type="text" id="name" />
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
           )}
 
           <div className="auth__field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
 
           <div className="auth__field">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
           {register && (
             <div className="auth__field">
               <label htmlFor="password">Confirm Password</label>
-              <input type="password" id="password" />
+              <input
+                type="password"
+                id="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
               {/* <p className="auth__error">
                 Something went wrong. Please try again.
               </p> */}
@@ -41,7 +105,9 @@ const AuthBox = ({ register }) => {
               Something went wrong. Please try again.
             </p>
 
-            <button className="btn">{register ? "Register" : "Login"}</button>
+            <button className="btn" type="submit" disabled={loading}>
+              {register ? "Register" : "Login"}
+            </button>
 
             {!register ? (
               <div className="auth__register">
